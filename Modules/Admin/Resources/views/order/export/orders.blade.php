fcdb16556160e480b627c9722c031b189a5cd1ed
jason.ly240 <jason.ly@at-com.com>
2024-10-07 14:25:36 +0700

    code update

--- Modules/Admin/Resources/views/order/export/orders.blade.php
+++ Modules/Admin/Resources/views/order/export/orders.blade.php
@@ -193,65 +193,65 @@
             <tr style="height: 19px">
                 <td class="s4">Mã phiếu</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Ngày tạo</div>
+                    <div class="softmerge-inner" style="left:-1px">Ngày tạo</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Thu ngân</div>
+                    <div class="softmerge-inner" style="left:-1px">Thu ngân</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Khách hàng</div>
+                    <div class="softmerge-inner" style="left:-1px">Khách hàng</div>
                 </td>
                 <td class="s4">Địa chỉ</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Điện thoại</div>
+                    <div class="softmerge-inner" style="left:-1px">Điện thoại</div>
                 </td>
                 <td class="s4">Tổng SL</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Tổng tiền hàng</div>
+                    <div class="softmerge-inner" style="left:-1px">Tổng tiền hàng</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Giảm giá (trên phiếu)</div>
+                    <div class="softmerge-inner" style="left:-1px">Giảm giá (trên phiếu)</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Tổng tiền</div>
+                    <div class="softmerge-inner" style="left:-1px">Tổng tiền</div>
                 </td>
                 <td class="s4">Tiền mặt</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Chuyển khoản</div>
+                    <div class="softmerge-inner" style="left:-1px">Chuyển khoản</div>
                 </td>
                 <td class="s4">Thẻ</td>
                 <td class="s4">Còn nợ</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Loại Đơn hàng</div>
+                    <div class="softmerge-inner" style="left:-1px">Loại Đơn hàng</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Trạng thái</div>
+                    <div class="softmerge-inner" style="left:-1px">Trạng thái</div>
                 </td>
                 <td class="s4">Ghi chú</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Đơn vị giao hàng</div>
+                    <div class="softmerge-inner" style="left:-1px">Đơn vị giao hàng</div>
                 </td>
                 <td class="s4">Phí VC</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Ngày giao</div>
+                    <div class="softmerge-inner" style="left:-1px">Ngày giao</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Người giao</div>
+                    <div class="softmerge-inner" style="left:-1px">Người giao</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Ghi chú vận chuyển</div>
+                    <div class="softmerge-inner" style="left:-1px">Ghi chú vận chuyển</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Tên hàng hóa</div>
+                    <div class="softmerge-inner" style="left:-1px">Tên hàng hóa</div>
                 </td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Mã hàng hóa</div>
+                    <div class="softmerge-inner" style="left:-1px">Mã hàng hóa</div>
                 </td>
                 <td class="s4">SL</td>
                 <td class="s4">Giá bán</td>
                 <td class="s4">Giảm giá</td>
                 <td class="s4 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">Thành tiền</div>
+                    <div class="softmerge-inner" style="left:-1px">Thành tiền</div>
                 </td>
                 <td class="s4">Serial</td>
                 <td class="s4"></td>
@@ -288,16 +288,16 @@
                 <td class="s1">{{$record->code}}</td>
                 <td class="s2">{{$record->date}}</td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{$record->id}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{$record->id}}</div>
                 </td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{$record->customer_id}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{$record->customer_id}}</div>
                 </td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{$record->shipping_address}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{$record->shipping_address}}</div>
                 </td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{$record->customer_phone}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{$record->customer_phone}}</div>
                 </td>
                 <td class="s2">1,030 </td>
                 <td class="s2">{{number_format($record->total)}}</td>
@@ -308,13 +308,13 @@
                 <td class="s2">{{ $record->payments_type==3?number_format($record->total_paid):0 }}</td>
                 <td class="s2">{{ $record->payments_type==2?number_format($record->total_paid):0 }}</td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{@$status[$record->status]}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{@$status[$record->status]}}</div>
                 </td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px"> {{@$status[$record->status]}}</div>
+                    <div class="softmerge-inner" style="left:-1px"> {{@$status[$record->status]}}</div>
                 </td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{$record->note}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{$record->note}}</div>
                 </td>
                 <td class="s1">{{$record->carrier_name}}</td>
                 <td class="s2">{{number_format($record->shipping_fee)}}</td>
@@ -328,10 +328,10 @@
                 $code = $record->code;
                 @endphp
                 <td class="s7 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{$record->description}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{$record->description}}</div>
                 </td>
                 <td class="s1 softmerge">
-                    <div class="softmerge-inner" style="width:57px;left:-1px">{{$record->sku}}</div>
+                    <div class="softmerge-inner" style="left:-1px">{{$record->sku}}</div>
                 </td>
                 <td class="s2">{{$record->qty}}</td>
                 <td class="s2">{{$record->sku}}</td>
